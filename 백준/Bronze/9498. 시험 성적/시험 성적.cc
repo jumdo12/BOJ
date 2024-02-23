d7e8bd6f75102c6dee85a4fddc7b7492fbc3eeeb
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-02-23 11:06:08 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int Score;
+    cin >> Score;
+    if(Score >= 90)  cout << "A";
+    else if(Score >= 80) cout << "B";
+    else if(Score >= 70) cout << "C";
+    else if(Score >= 60) cout << "D";
+    else cout << "F";
+
+    return 0;
+}
