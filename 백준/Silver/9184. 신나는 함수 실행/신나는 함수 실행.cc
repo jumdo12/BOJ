2ce738eedf8b16ec61d210c5a8a9529738a6908e
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-03-18 22:45:08 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 428 ms, Memory: 2540 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -1,33 +1,25 @@
 #include <iostream>
 
+int w[51][51][51] = {1,};
+
+int W(int a, int b, int c){
+    if(a<=0||b<=0||c<=0)    return 1;
+    if(a>20||b>20||c>20)    return W(20,20,20);
+
+    if(w[a][b][c])  return w[a][b][c];
+    if(a<b&&b<c)    w[a][b][c] = W(a,b,c-1) + W(a,b-1,c-1) - W(a,b-1,c);
+    else    w[a][b][c] = W(a-1,b,c)+W(a-1,b-1,c)+W(a-1,b,c-1)-W(a-1,b-1,c-1);
+
+    return w[a][b][c];
+}
+
 int main(){
-    int A,B,C;
-    std::cin>>A>>B>>C;
+    int a,b,c;
+    while(1){
+        std::cin>>a>>b>>c;
+        if(a == -1 && b == -1 && c == -1)   break;
 
-    int w[101][101][101] = {0};
-
-    for (int a = 0; a < 101; a++){
-        for(int b=0; b<101; b++){
-            for(int c=0; c<101; c++){
-                if(a <= 0 + 50 || b <= 0 + 50 || c <= 0 + 50){
-                    w[a][b][c] = 1;
-                }
-                else if(a > (20 + 50) || b > (20 + 50) || c > (20 + 50)){
-                    w[a][b][c] = 1048576;
-                }
-                else if(a<b&&b<c){
-                    w[a][b][c] = w[a][b][c-1] + w[a][b-1][c-1] - w[a][b-1][c];
-                }
-                else{
-                    w[a][b][c] = w[a-1][b][c] + w[a-1][b-1][c] + w[a-1][b][c-1] - w[a-1][b-1][c-1];
-                }
-            }
-        }
-    }
-
-    while(A != -1 || B != -1 || C != -1){
-        std::cout<<"w("<<A<<", "<<B<<", "<<C<< ") = "<< w[A+50][B+50][C+50]<<'\n';
-        std::cin>>A>>B>>C;
+        std::cout<<"w("<<a<<", "<<b<<", "<<c<<") = "<<W(a,b,c)<<'\n';
     }
     return 0;
 }
