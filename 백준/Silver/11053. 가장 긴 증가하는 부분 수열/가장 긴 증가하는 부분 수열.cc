1505e7057c4f64495cb1bc15bc01601e2c5a53a9
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-03-30 16:22:56 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -19,7 +19,7 @@ int main(){
     ans = 0;
     for(int i=1; i<=N; i++){
         dp[i] = 1;
-        for(int j = i-1; j>=1; j--){
+        for(int j = 1; j<i; j++){
             if(arr[j] < arr[i]){
                 dp[i] = max(dp[i],dp[j]+1);
             }
