6a7d793491256ad9f9bc278bbfcf2f8fd1dbcfea
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-05-15 20:47:05 +0900

    [Platinum V] Title: 히스토그램, Time: 44 ms, Memory: 4348 KB -BaekjoonHub

---
+++ 백준/Platinum/1725. 히스토그램/히스토그램.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+typedef long long ll;
+
+std::vector<ll> v;
+ll arr[100002] = {0};
+
+ll max(ll a, ll b){
+    return a > b ? a : b;
+}
+
+int main(){
+    int cnt;
+    ll ans = 0;
+    ll w;
+    
+    std::cin>>cnt;
+    
+    v.push_back(0);
+
+    for(int i =1; i<=cnt; i++){
+        std::cin>>arr[i];
+    }
+
+    for(int i=1; i<=cnt+1; i++){
+        while(!v.empty() && arr[v.back()] > arr[i]){
+            w = v.back();
+            v.pop_back();
+            ans = max(ans,arr[w] * (i-1-v.back()));
+        }
+        v.push_back(i);
+    }
+    std::cout << ans;
+    
+    return 0;
+}
