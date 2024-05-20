c7a63a8298cec95c5495c08f10a133a892943b69
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-05-20 20:58:50 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 224 ms, Memory: 31888 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+std::vector<int> graph[1000001];
+int visited[100001] = {0};
+int cnt = 1;
+
+void DFS(int S)
+{
+    if (!visited[S])
+    {
+        visited[S] = cnt++;
+        for (int i = 0; i < graph[S].size(); i++)
+        {
+            DFS(graph[S][i]);
+        }
+    }
+    return;
+}
+
+int main()
+{
+    int src, dest, V, E, S;
+
+    std::cin >> V >> E >> S;
+
+    for (int i = 0; i < E; i++)
+    {
+        std::cin >> src >> dest;
+        graph[src].push_back(dest);
+        graph[dest].push_back(src);
+    }
+
+    for (int i = 1; i <= V; i++)
+    {
+        std::sort(graph[i].rbegin(), graph[i].rend());
+    }
+
+    DFS(S);
+
+    for (int i = 1; i <= V; i++)
+    {
+        std::cout << visited[i] << '\n';
+    }
+
+    return 0;
+}
