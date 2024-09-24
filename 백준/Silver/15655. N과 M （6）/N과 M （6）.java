2b4b0ef8aca3712a54c08402773fbb7d60d95cf8
jumdo12 <126929413+jumdo12@users.noreply.github.com>
2024-09-24 20:12:14 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 15904 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int arr[];
+    static int ans[];
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public void solve() throws Exception{
+
+
+        StringTokenizer sTokenizer;
+        sTokenizer = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(sTokenizer.nextToken());
+        M = Integer.parseInt(sTokenizer.nextToken());
+
+        arr = new int[N];
+        ans = new int[M];
+
+        sTokenizer = new StringTokenizer(br.readLine());
+        for(int i=0; i < N; i++){
+            arr[i] = Integer.parseInt(sTokenizer.nextToken());
+        }
+
+        Arrays.sort(arr);
+        DFS(0,0);
+    }
+
+    public static void DFS(int curr, int idx) throws Exception{
+        if(curr == M){
+            for(int i=0; i<M; i++){
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        } 
+        else {
+            for(int i = idx; i< N; i++){
+                ans[curr] = arr[i];
+                DFS(curr+1,i+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        new Main().solve();
+    }
+}
