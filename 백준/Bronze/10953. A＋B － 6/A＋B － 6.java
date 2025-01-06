33a1d2de0d94229cc141d2549e4b7eeef8e9580a
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-01-07 02:35:59 +0900

    [Bronze III] Title: A+B - 6, Time: 108 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -4,15 +4,15 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n;
-        String line;
         try {
-            n = Integer.parseInt(bf.readLine());
-            for(int i=0; i<n; i++){
-                line = bf.readLine();
-                String[] parsedLine = line.split(",");
-                System.out.println(Integer.parseInt(parsedLine[0]) + Integer.parseInt(parsedLine[1]));
-            }
+           int n = Integer.parseInt(bf.readLine());
+           for(int i=0; i<n; i++){
+               String line = bf.readLine();
+               String[] parsedLine = line.split(",");
+               int a = Integer.parseInt(parsedLine[0]);
+               int b = Integer.parseInt(parsedLine[1]);
+               System.out.println(a+b);
+           }
         }
         catch (Exception e){
             e.printStackTrace();
