dda2eddca10fd3a5d79116926724f01c60dc8d6f
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-01-08 18:00:08 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1232 ms, Memory: 90040 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        solve();
+    }
+
+    private static void solve(){
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, String> passwords = new HashMap<>();
+
+        try {
+            String line = bf.readLine();
+            int tCount = Integer.parseInt(line.split(" ")[0]);
+            int caseCount = Integer.parseInt(line.split(" ")[1]);
+
+            for(int i=0; i<tCount; i++){
+                line = bf.readLine();
+                String name = line.split(" ")[0];
+                String password = line.split(" ")[1];
+
+                passwords.put(name,password);
+            }
+
+            for(int i=0; i<caseCount; i++){
+                line = bf.readLine();
+                System.out.println(passwords.get(line));
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
