e794ed873869a78067aeb2b96e0f2167d3d0017c
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-01-20 01:12:46 +0900

    [level 0] Title: 공배수, Time: 0.02 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.java
+++ 프로그래머스/0/181936. 공배수/공배수.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int number, int n, int m) {
-        if(number % n == 0 && number % m == 0) return 1;
-        return 0;
+        int answer = 0;
+        return answer;
     }
 }
