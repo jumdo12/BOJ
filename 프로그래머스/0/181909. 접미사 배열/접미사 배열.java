e3ba08cf9b5bdc7a033c406c96ea8faa4ee9d5f3
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-01-28 20:31:37 +0900

    [level 0] Title: 접미사 배열, Time: 0.95 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        List<String> list = new ArrayList<>();
+        for(int i=0; i<my_string.length(); i++){
+            list.add(my_string.substring(i));
+        }
+        list.sort(Comparator.naturalOrder());
+        
+        return list.toArray(new String[0]);
+    }
+}
