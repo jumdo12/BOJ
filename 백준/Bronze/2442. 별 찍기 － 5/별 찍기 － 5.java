a586cc77d91760c517ab1e6e18b45794b7c9ffa1
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-09-07 13:20:43 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void solve(BufferedReader bf) throws IOException {
+        String line = bf.readLine();
+        int n = Integer.parseInt(line);
+
+        for(int i=0; i<n; i++) {
+            for(int j=n-i; j > 1; j--) {
+                sb.append(" ");
+            }
+            for(int j = 0; j <= i; j++) {
+                sb.append("*");
+            }
+            for(int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try{
+            solve(br);
+
+            bw.write(sb.toString());
+            bw.flush();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
