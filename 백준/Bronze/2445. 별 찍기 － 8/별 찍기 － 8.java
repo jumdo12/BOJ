05eaa8a1e8e6c1fb97e061f624a27fe2c449a029
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-09-07 13:35:40 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 112 ms, Memory: 14424 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void solve(BufferedReader bf) throws IOException {
+        String line = bf.readLine();
+        int n = Integer.parseInt(line);
+
+        for(int i=0; i<n; i++) {
+            for(int j = 0; j < i+1; j++) {
+                sb.append("*");
+            }
+            for(int j = 2*(n-i)-1; j > 1; j--) {
+                sb.append(" ");
+            }
+            for(int j = 0; j < i+1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        for(int i=0; i<n; i++) {
+            for(int j = i+1; j < n; j++) {
+                sb.append("*");
+            }
+            for(int j = 0; j < 2*(i+1); j++) {
+                sb.append(" ");
+            }
+            for(int j = i+1; j < n; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        try{
+            solve(br);
+
+            bw.write(sb.toString());
+            bw.flush();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
