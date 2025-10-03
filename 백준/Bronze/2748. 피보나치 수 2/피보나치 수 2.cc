35ab6c5eb182ffe181927306d80867d11b558a90
SeungYeon <126929413+jumdo12@users.noreply.github.com>
2025-10-03 15:34:28 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[100] = {0};
+
+int main() {
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
+
+    int n;
+    cin >> n;
+
+    for(int i = 3; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    cout << dp[n];
+}
